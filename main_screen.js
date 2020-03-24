53e05f13693034bcb9d35bed59bb894b20c98f88
github username <github email address>
2020-03-24 14:51:15 -0400

    pulled from master

--- main_screen.js
+++ main_screen.js
@@ -13,7 +13,7 @@ function turnON() {
       })
       .then(function (pokemons) {
         console.log(pokemons);
-      })
+      });
   });
 }
 
@@ -23,60 +23,60 @@ function turnON() {
 
 function fetchTeams() {
   fetch('http://localhost:3000/teams')
-  .then(response => response.json())
-  .then(json => displayTeams(json))   
+    .then((response) => response.json())
+    .then((json) => displayTeams(json));
 }
 
 function displayTeams(teams) {
-  const team1List = document.getElementById("team-1-list")
-  const team2List = document.getElementById("team-2-list")
-  const team3List = document.getElementById("team-3-list")
-  const team4List = document.getElementById("team-4-list")
-  const team5List = document.getElementById("team-5-list")
-  const team6List = document.getElementById("team-6-list")
+  const team1List = document.getElementById('team-1-list');
+  const team2List = document.getElementById('team-2-list');
+  const team3List = document.getElementById('team-3-list');
+  const team4List = document.getElementById('team-4-list');
+  const team5List = document.getElementById('team-5-list');
+  const team6List = document.getElementById('team-6-list');
 
-  teams[0].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team1List.appendChild(li)
+  teams[0].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team1List.appendChild(li);
   });
 
-  teams[1].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team2List.appendChild(li)
+  teams[1].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team2List.appendChild(li);
   });
 
-  teams[2].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team3List.appendChild(li)
+  teams[2].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team3List.appendChild(li);
   });
 
-  teams[3].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team4List.appendChild(li)
+  teams[3].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team4List.appendChild(li);
   });
 
-  teams[4].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team5List.appendChild(li)
+  teams[4].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team5List.appendChild(li);
   });
 
-  teams[5].pokemons.forEach(pokemon => {
-    const li = document.createElement("li")
-    li.innerText = pokemon.name
-    team6List.appendChild(li)
+  teams[5].pokemons.forEach((pokemon) => {
+    const li = document.createElement('li');
+    li.innerText = pokemon.name;
+    team6List.appendChild(li);
   });
 }
 
 function battleRender() {
-  document.addEventListener("click", function (event) {
-    if (event.target.className === "team-button") {
-      const teamsContainer = document.getElementById("teams-container")
-      teamsContainer.innerHTML = ``
+  document.addEventListener('click', function (event) {
+    if (event.target.className === 'team-button') {
+      const teamsContainer = document.getElementById('teams-container');
+      teamsContainer.innerHTML = ``;
     }
-  })
+  });
 }
