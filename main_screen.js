d65920e251300638e5f1db495d95f14e29ab3fe7
haltonba <haltonba94@gmail.com>
2020-03-24 14:36:30 -0400

    div_setup

--- main_screen.js
+++ main_screen.js
@@ -3,38 +3,80 @@ turnON();
 
 function turnON() {
   document.addEventListener('DOMContentLoaded', function () {
-    displayTeams();
+    document.body.style.backgroundImage = "url('main2.jpg')";
+    fetchTeams();
+    battleRender();
+
     fetch('http://localhost:3000/pokemons')
       .then(function (response) {
         return response.json();
       })
       .then(function (pokemons) {
         console.log(pokemons);
-      });
+      })
   });
 }
 
 //set background image when  render
 //create a new  div "team-container", and displays teams  with innerHTML.
 //display all the teams
-function displayTeams() {
-  document.body.style.backgroundImage = "url('main2.jpg')";
 
-  let div = document.createElement('div');
-  div.className = 'team-container';
+function fetchTeams() {
+  fetch('http://localhost:3000/teams')
+  .then(response => response.json())
+  .then(json => displayTeams(json))   
+}
 
-  div.innerHTML += `
-    <div class="div1">
-        <ul>    
-        Temas  will display here
-        <li data-id="$team.id">Team 1</li>
-        <li data-id="$team.id">Team 2</li>
-        <li data-id="$team.id">Team 3</li>
-        <li data-id="$team.id">Team 4</li>
-        <li data-id="$team.id">Team 5</li>
-        <li data-id="$team.id">Team 6</li>
-        </ul>
-    </div>
-    `;
-  document.body.append(div);
+function displayTeams(teams) {
+  const team1List = document.getElementById("team-1-list")
+  const team2List = document.getElementById("team-2-list")
+  const team3List = document.getElementById("team-3-list")
+  const team4List = document.getElementById("team-4-list")
+  const team5List = document.getElementById("team-5-list")
+  const team6List = document.getElementById("team-6-list")
+
+  teams[0].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team1List.appendChild(li)
+  });
+
+  teams[1].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team2List.appendChild(li)
+  });
+
+  teams[2].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team3List.appendChild(li)
+  });
+
+  teams[3].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team4List.appendChild(li)
+  });
+
+  teams[4].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team5List.appendChild(li)
+  });
+
+  teams[5].pokemons.forEach(pokemon => {
+    const li = document.createElement("li")
+    li.innerText = pokemon.name
+    team6List.appendChild(li)
+  });
+}
+
+function battleRender() {
+  document.addEventListener("click", function (event) {
+    if (event.target.className === "team-button") {
+      const teamsContainer = document.getElementById("teams-container")
+      teamsContainer.innerHTML = ``
+    }
+  })
 }
